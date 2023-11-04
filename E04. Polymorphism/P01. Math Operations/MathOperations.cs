61f36f03f406ca4d88be9da48edb34d0e65897a7
Kolio Kolev <114016354+KStKolev@users.noreply.github.com>
2023-11-04 14:46:31 +0200

    Add files via upload

---
+++ E04. Polymorphism/P01. Math Operations/MathOperations.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operations
+{
+    public class MathOperations
+    {
+        public int Add(int firstValue, int secondValue) 
+        {
+            return firstValue + secondValue;
+        }
+        public double Add(double firstValue, double secondValue, double thirdValue) 
+        {
+            return firstValue + secondValue + thirdValue;
+        }
+        public decimal Add(decimal firstValue, decimal secondValue, decimal thirdValue) 
+        {
+            return firstValue + secondValue + thirdValue;
+        }
+    }
+}
---
+++ E04. Polymorphism/P02. Animals/Animal.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animals
+{
+    public class Animal
+    {
+        private string name;
+        private string favouriteFood;
+
+        public Animal(string name, string favouriteFood)
+        {
+            Name = name;
+            FavouriteFood = favouriteFood;
+        }
+
+        public string Name { get => name; private set => name = value; }
+        public string FavouriteFood { get => favouriteFood; private set => favouriteFood = value; }
+
+        public virtual string ExplainSelf() 
+        {
+            return "";
+        }
+
+    }
+}
