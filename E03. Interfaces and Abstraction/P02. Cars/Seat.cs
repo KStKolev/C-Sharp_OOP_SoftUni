1547ba38764f55edae1a8bca24ee2b463cf5cfad
Kolio Kolev <114016354+KStKolev@users.noreply.github.com>
2023-11-04 14:44:13 +0200

    Add files via upload

---
+++ E03. Interfaces and Abstraction/P02. Cars/Seat.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars
+{
+    public class Seat : ICar
+    {
+        private string color;
+        private string model;
+
+        public Seat(string model, string color)
+        {
+            this.model = model;
+            this.color = color;
+        }
+
+        public string Color()
+        {
+            return $"{color}";
+        }
+
+        public string Model()
+        {
+            return $"{model}";
+        }
+
+        public string Start()
+        {
+            return "Engine start";
+        }
+
+        public string Stop()
+        {
+            return "Breaaak!";
+        }
+
+        public override string ToString()
+        {
+            StringBuilder seat = new StringBuilder();
+            seat.Append($"{Color()} Seat {Model()} ");
+            seat.AppendLine();
+            seat.AppendLine(Start());
+            seat.Append(Stop());
+            return seat.ToString();
+        }
+    }
+}
---
+++ E03. Interfaces and Abstraction/P02. Cars/Tesla.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars
+{
+    public class Tesla : ICar, IElectricBattery
+    {
+        private string color;
+        private string model;
+        private int battery;
+
+        public Tesla(string model, string color, int battery)
+        {
+            this.model = model;
+            this.color = color;
+            this.battery = battery;
+        }
+
+        public int Battery()
+        {
+            return battery;
+        }
+
+        public string Color()
+        {
+            return $"{color}";
+        }
+
+        public string Model()
+        {
+            return $"{model}";
+        }
+
+        public string Start()
+        {
+            return "Engine start";
+        }
+
+        public string Stop()
+        {
+            return "Breaaak!";
+        }
+
+        public override string ToString()
+        {
+            StringBuilder tesla = new StringBuilder();
+            tesla.Append($"{Color()} Tesla {Model()} with {Battery()} Batteries ");
+            tesla.AppendLine();
+            tesla.AppendLine(Start());
+            tesla.Append(Stop());
+            return tesla.ToString();
+        }
+    }
+}
